ebcd8e895fbccac680405448e4cbf74ef7362937
Nguyễn Thiên Trường <xayda.ln@gmail.com>
2026-06-08 23:39:05 +0700

    Add feature create job and review proposal by business & review job and submit proposal by expert

--- src/main/java/com/aitasker/be/repository/ProposalRepository.java
+++ src/main/java/com/aitasker/be/repository/ProposalRepository.java
@@ -1,3 +1,8 @@
+/*
+ * NOTE FILE: src/main/java/com/aitasker/be/repository/ProposalRepository.java
+ * Đây là file gì: File repository định nghĩa cổng truy cập dữ liệu, để Spring Data JPA sinh truy vấn tới database.
+ * Mục đích note: giải thích các annotation và hàm chính để đọc hiểu chức năng code.
+ */
 package com.aitasker.be.repository;
 
 import com.aitasker.be.entity.ProposalEntity;
@@ -7,5 +12,8 @@ import java.util.List;
 
 public interface ProposalRepository extends JpaRepository<ProposalEntity, Integer> {
     boolean existsByJobIdAndExpertId(Integer jobId, Integer expertId);
+    // Note: Hàm `findByJobId` khai báo truy vấn dữ liệu để Spring Data JPA tự sinh logic truy cập database.
     List<ProposalEntity> findByJobId(Integer jobId);
+    // Note: Hàm `findByExpertIdOrderByCreatedAtDesc` lấy các proposal của chuyên gia hiện tại để expert theo dõi lịch sử đã nộp.
+    List<ProposalEntity> findByExpertIdOrderByCreatedAtDesc(Integer expertId);
 }
